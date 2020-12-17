781006b0ea54ad88ae75906f686fe99cdb06a4ae
Andrey_Ponomarev <ANDRUXA.51.RUS@yandex.ru>
2020-12-17 19:23:38 +0300

    feat: Add new middlewares for game process fix: Fix connect, registration, game middlewares to avoid arrays errors and enable async data exchange

--- internal/app/apiserver/server.go
+++ internal/app/apiserver/server.go
@@ -43,12 +43,18 @@ func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func (s *server) configureRouter() {
-	s.router.Use(handlers.CORS(handlers.AllowedOrigins([]string{"*"})))
+	s.router.Use(handlers.CORS(handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"}), handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS"}), handlers.AllowedOrigins([]string{"*"})))
+
 	s.router.Use(jwt.JwtAuthentication)
 	s.router.Use(s.logRequest)
 	s.router.HandleFunc("/user/new", s.createUser()).Methods("POST")
 	s.router.HandleFunc("/user/login", s.logUser()).Methods("POST")
 	s.router.HandleFunc("/lobby/create", s.createLobby()).Methods("POST")
+	s.router.HandleFunc("/lobby/adminconnect/{token}", s.connectLobby()).Methods("POST")
+	s.router.HandleFunc("/lobby/connect/{token}", s.connectLobby()).Methods("POST")
+	s.router.HandleFunc("/lobby/start/{token}", s.startGame()).Methods("POST")
+	s.router.HandleFunc("/lobby/checkresult/{token}", s.checkResult()).Methods("GET")
+	s.router.HandleFunc("/lobby/checklocation/{token}", s.checkLocation()).Methods("POST")
 }
 
 func (s *server) logRequest(next http.Handler) http.Handler {
@@ -113,7 +119,8 @@ func (s *server) createUser() http.HandlerFunc {
 		}
 
 		if err := s.store.User().Create(usermodel); err != nil {
-			u.Error(w, http.StatusUnprocessableEntity, err)
+			response := u.Message(false, "Error while creating user")
+			u.Respond(w, response)
 			return
 		}
 
@@ -168,7 +175,7 @@ func (s *server) logUser() http.HandlerFunc {
 			return
 		}
 
-		err = bcrypt.CompareHashAndPassword([]byte(req.Password), []byte(loc.Password))
+		err = bcrypt.CompareHashAndPassword([]byte(loc.Password), []byte(usermodel.Password))
 		if err != nil && err == bcrypt.ErrMismatchedHashAndPassword {
 			response := u.Message(false, "Invalid login credentials. Please try again")
 			u.Respond(w, response)
@@ -231,8 +238,225 @@ func (s *server) createLobby() http.HandlerFunc {
 			return
 		}
 
+		currentlobby.CurrentLocation = ""
+
 		response := u.Message(true, "Lobby has been created")
 		response["lobby"] = currentlobby
+		u.Respond(w, response)
+	}
+}
+
+// connectLobby func
+func (s *server) connectLobby() http.HandlerFunc {
+
+	type request struct {
+		Login string `json:"login"`
+	}
+
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		req := &request{}
+
+		vars := mux.Vars(r)
+		token := vars["token"]
+
+		if err := json.NewDecoder(r.Body).Decode(req); err != nil {
+			u.Error(w, http.StatusBadRequest, err)
+			return
+		}
+
+		currentlobby, err := s.store.Lobby().FindByToken(token)
+		if err != nil {
+			u.Error(w, http.StatusUnprocessableEntity, err)
+			return
+		}
+
+		if status, err := s.store.Lobby().CheckStatus(token); status == "Started" && err == nil {
+			response := u.Message(false, "Game has started already, you can't enter")
+			u.Respond(w, response)
+			return
+		}
+
+		currentlobby.AllPlayers = append(currentlobby.AllPlayers, req.Login)
+
+		err = s.store.Lobby().ConnectUserToLobby(currentlobby)
+		if err != nil {
+			u.Error(w, http.StatusUnprocessableEntity, err)
+			return
+		}
+
+		for {
+			if status, err := s.store.Lobby().CheckStatus(token); status == "Started" && err == nil {
+
+				connectedlobby, err := s.store.Lobby().FindByToken(token)
+				if err != nil {
+					u.Error(w, http.StatusUnprocessableEntity, err)
+					return
+				}
+
+				if flag := u.Contains(connectedlobby.SpyPlayers, req.Login); flag == true {
+					var cleararray []string
+					connectedlobby.CurrentLocation = ""
+					connectedlobby.SpyPlayers = cleararray
+					response := u.Message(true, "Game has started, you are spy")
+					response["lobby"] = connectedlobby
+					u.Respond(w, response)
+				} else {
+					var cleararray []string
+					connectedlobby.SpyPlayers = cleararray
+					response := u.Message(true, "Game has started, you are peaceful")
+					response["lobby"] = connectedlobby
+					u.Respond(w, response)
+				}
+				break
+			}
+		}
+	}
+}
+
+// checkLocation func
+func (s *server) checkLocation() http.HandlerFunc {
+
+	type checkrequest struct {
+		Login    string `json:"login"`
+		Location string `json:"location"`
+	}
+
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		vars := mux.Vars(r)
+		token := vars["token"]
+
+		cheklocreq := &checkrequest{}
+
+		if err := json.NewDecoder(r.Body).Decode(cheklocreq); err != nil {
+			u.Error(w, http.StatusBadRequest, err)
+			return
+		}
+
+		connectedlobby, err := s.store.Lobby().FindByToken(token)
+		if err != nil {
+			u.Error(w, http.StatusUnprocessableEntity, err)
+			return
+		}
+
+		if flag := u.Contains(connectedlobby.SpyPlayers, cheklocreq.Login); flag == true {
+			if connectedlobby.CurrentLocation == cheklocreq.Location {
+				result, err := s.store.Lobby().WonForSpy(connectedlobby)
+				if err != nil {
+					response := u.Message(false, "Unavailiable to end game for spy")
+					u.Respond(w, response)
+					return
+				}
+				response := u.Message(true, result)
+				response["lobby"] = connectedlobby
+				u.Respond(w, response)
+			} else {
+				result, err := s.store.Lobby().WonForPeaceful(connectedlobby)
+				if err != nil {
+					response := u.Message(false, "Unavailiable to end game for peaceful")
+					u.Respond(w, response)
+					return
+				}
+				response := u.Message(true, result)
+				response["lobby"] = connectedlobby
+				u.Respond(w, response)
+			}
+		} else {
+			response := u.Message(false, "Misha, a ti krasava")
+			u.Respond(w, response)
+		}
+		return
+	}
+}
+
+func (s *server) checkResult() http.HandlerFunc {
+
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		vars := mux.Vars(r)
+		token := vars["token"]
+
+		for {
+			if status, err := s.store.Lobby().CheckStatus(token); status == "Spy won" && err == nil {
+
+				connectedlobby, err := s.store.Lobby().FindByToken(token)
+				if err != nil {
+					u.Error(w, http.StatusUnprocessableEntity, err)
+					return
+				}
+
+				response := u.Message(true, status)
+				response["lobby"] = connectedlobby
+				u.Respond(w, response)
+
+				break
+			} else if status, err := s.store.Lobby().CheckStatus(token); status == "Peaceful won" && err == nil {
+
+				connectedlobby, err := s.store.Lobby().FindByToken(token)
+				if err != nil {
+					u.Error(w, http.StatusUnprocessableEntity, err)
+					return
+				}
+
+				response := u.Message(true, status)
+				response["lobby"] = connectedlobby
+				u.Respond(w, response)
+
+				break
+			}
+		}
+	}
+}
+
+// startGame func
+func (s *server) startGame() http.HandlerFunc {
+
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		vars := mux.Vars(r)
+		token := vars["token"]
+
+		currentlobby, err := s.store.Lobby().FindByToken(token)
+		if err != nil {
+			u.Error(w, http.StatusUnprocessableEntity, err)
+			return
+		}
+
+		playersarray := currentlobby.AllPlayers
+
+		if len(playersarray) != currentlobby.AmountPl {
+			response := u.Message(false, "Unable to start game, not enough players")
+			u.Respond(w, response)
+			return
+		}
+
+		var spyplayers []string = make([]string, 0, 20)
+		j := currentlobby.AmountPl
+
+		for i := currentlobby.AmountSpy; i > 0; i-- {
+			a := rand.Intn(j)
+			spyplayers = append(spyplayers, playersarray[a])
+			playersarray = append(playersarray[:a], playersarray[a+1:]...)
+			j--
+		}
+
+		currentlobby.SpyPlayers = spyplayers
+
+		err = s.store.Lobby().ChooseSpyPlayersInLobby(currentlobby)
+		if err != nil {
+			u.Error(w, http.StatusUnprocessableEntity, err)
+			return
+		}
+
+		err = s.store.Lobby().StartGame(currentlobby)
+		if err != nil {
+			response := u.Message(false, "Unable to start game")
+			u.Respond(w, response)
+			return
+		}
+
+		response := u.Message(true, "Game has started")
 		u.Respond(w, response)
 	}
 }
